5d3784c88dd56a169bf89c1997f778964a38bf7d
Naos2w <simplelove0922@yahoo.com.tw>
2025-06-03 18:41:58 +0800

    19-integrated-hooks-app

---
+++ 0-now-project/src/context/ThemeContext.tsx
@@ -0,0 +1,31 @@
+import { useState, createContext, useEffect } from "react";
+import type { ThemeContextType } from "../types";
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+};
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "Dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
---
+++ 19-integrated-hooks-app/src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import type { ToDoListType, Action } from "../types";
+import { todoReducer } from "../reducer/todoReducer";
+type TodoListProps = {
+  todos: ToDoListType[];
+  dispatch: React.Dispatch<Action>;
+};
+export const TodoList: React.FC<TodoListProps> = ({ todos, dispatch }) => {
+  console.log(`ToDoList: ${todos.length}`);
+  return (
+    <>
+      {todos.map((todo, idx) => {
+        return (
+          <div key={idx} className="card">
+            <input
+              className="card_check"
+              type="checkbox"
+              checked={todo.checked}
+              title="Completed"
+              onChange={(e) => {
+                dispatch({
+                  type: "TOGGLE_TODO",
+                  idx: idx,
+                });
+              }}
+            />
+            <div className="card_title">{todo.title}</div>
+            <p className="card_content">{todo.content}</p>
+            <div className="card_time">{todo.addtime}</div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
